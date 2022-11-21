d3d58271ddbdb3ba2c11731c0776205069a22c21
pistonsoup <62525293+pistonsoup@users.noreply.github.com>
2022-11-20 20:55:55 -0800

    Strap down and Linear Algebra

---
+++ unit_conversions.h
@@ -0,0 +1,30 @@
+/*
+Unit Conversion factors 0.1 
+
+Date: 11.20.22
+
+Based on SI Meter and Imperial Inch
+
+copied by : Joe Wilson
+
+Coded as needed in personal projects
+
+Source:
+    https://www.nist.gov/pml/special-publication-811/nist-guide-si-appendix-b-conversion-factors/nist-guide-si-appendix-b9#ENERGY
+
+
+*/
+
+
+
+
+/* Base Units */
+#define _in2m_ = 0.0254
+#define _m2in_ = 1 / _in2m_
+
+#define _lbm2kg_ = 0.45
+#define _kg2lbm_ = 1/ _kg2lbm_
+
+#define _inlbf2J_ = 0.11298483333
+#define _J2inlbf_ = 1/_inlbf2J_
+
